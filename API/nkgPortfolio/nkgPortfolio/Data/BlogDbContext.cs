0bedb3fdaa8ad092c9fac8c5ff0ddedd00b27702
nickolas <nickolaskg@gmail.com>
2024-03-25 15:52:45 -0400

    Initialized Project
    Added ASP.NET WebAPI for the backend
    
    Added Angular app for the frontend

---
+++ API/nkgPortfolio/nkgPortfolio/Data/BlogDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nkgPortfolio.Models.Domain;
+
+namespace nkgPortfolio.Data
+{
+    public class BlogDbContext: DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> options): base(options)
+        {
+        }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+    }
+}
---
+++ API/nkgPortfolio/nkgPortfolio/Migrations/20240325154709_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace nkgPortfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BlogPosts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PublishedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UrlHandle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FeaturedImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    isVisible = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BlogPosts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BlogPosts");
+        }
+    }
+}
